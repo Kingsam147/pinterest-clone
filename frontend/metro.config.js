0a5ef08903567e936e9a09d0e50ad4027106e2b2
Kingsam147 <Samueldarius1470@gmail.com>
2025-04-30 15:53:17 -0400

    redid the front end folder for web

--- frontend/metro.config.js
+++
@@ -1,9 +0,0 @@
-const { getDefaultConfig } = require('expo/metro-config');
-
-const config = getDefaultConfig(__dirname);
-
-// Limit the number of files being watched
-config.watchFolders = [__dirname];
-config.resolver.nodeModulesPaths = [__dirname + '/node_modules'];
-
-module.exports = config; 
